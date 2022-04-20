1760e8cf3ea709dd97363753dbfe269cc2366ec5
ProBatou <baptiste@probatou.com>
2022-04-20 18:07:50 +0200

    change path

--- compile_asuswrt.sh
+++ compile_asuswrt.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-version=$(curl --silent https://www.asuswrt-merlin.net/  | perl -ln0e '/<table.*?table>/s;print $&' | grep -A 3 -B 1 RT-AX56U | html2text | sed '2!d')
-path="/home/root2/asuswrt"
-
 BOTNAME=Build-Notify
 AVATAR_URL="https://a.fsdn.com/allura/p/asuswrt-merlin/icon?1561187555?&w=90"
 WEBHOOK="https://discord.com/api/webhooks/964958022826868766/IblcZagFYogtPJjfsK-rLqtkwOaaIzu3BimhUEIlKOty7p1SpGe_1jlEvKVW4zi7tq7z"
@@ -10,7 +7,9 @@ DATE=$(date +"%d/%m/%Y")
 HEURE=$(date +"%H:%M:%S")
 getCurrentTimestamp() { date -u --iso-8601=seconds; };
 
+path=$PWD
 latestVersion=$(cat $path/version.txt)
+version=$(curl --silent https://www.asuswrt-merlin.net/  | perl -ln0e '/<table.*?table>/s;print $&' | grep -A 3 -B 1 RT-AX56U | html2text | sed '2!d')
 
 if [ "$latestVersion" = "$version" ]; then
     echo "Version is equal"
@@ -20,18 +19,18 @@ else
 
 wget -q https://codeload.github.com/RMerl/asuswrt-merlin.ng/tar.gz/refs/tags/$version
 
-echo "Downlad finish"
+echo "Downlad finished"
 
 if [ -d "$path/amng-build/" ]; then
-  echo "Already exist folder"
+  echo "Folder exist"
 else
   mkdir $path/amng-build
-  echo "Create folder finish"
+  echo "Folder created"
 fi
 
 tar -xf $version --strip 1 -C $path/amng-build && rm $path/$version
 
-echo "Tar finish"
+echo "Tar extracted"
 
 sed -i '/X-Frame-Options/d' $path/amng-build/release/src/router/curl/tests/data/test1270
 sed -i '/X-Frame-Options/d' $path/amng-build/release/src/router/lighttpd-1.4.39/src/response.c
@@ -39,28 +38,27 @@ sed -i '/X-Frame-Options/d' $path/amng-build/release/src/router/samba-3.6.x/sour
 sed -i '/X-Frame-Options/d' $path/amng-build/release/src/router/samba-3.0.33/source/web/swat.c
 sed -i '/x-frame-options/d' $path/amng-build/release/src/router/httpd/httpd.c
 sed -i '/X-Frame-Options/d' $path/amng-build/release/src/router/vsftpd-3.x/postlogin.c
-
 sed -i '/x-xss-protection/d' $path/amng-build/release/src/router/httpd/httpd.c
 
-echo "Modify file finish"
+echo "Frame removed"
 
 cd $path/amng-build/release/src-rt-5.02axhnd.675x/ && /usr/bin/make -s --no-print-directory rt-ax56u
 
 error=$?
 
-sudo rm /var/www/html/*
+sudo rm /var/www/html/asuswrt/*
 
-sudo cp $(find $path/amng-build/release/src-rt-5.02axhnd.675x/ -name *_cferom_pureubi.w) /var/www/html/
+sudo cp $(find $path/amng-build/release/src-rt-5.02axhnd.675x/ -name *_cferom_pureubi.w) /var/www/html/asuswrt/
+
+changelog=$(sed -e "s/\r//g" $path/amng-build/Changelog-NG.txt | sed -n "/$version/,/^$/{/./p}" | sed -e "s/$/\\\\n /g" | tr '\n' ' ')
 
 rm -r $path/amng-build/
 
-echo "Make finish"
+echo "Build finished"
 
 if [ $error = 0 ]; then
 
 sed -i "s/$latestVersion/$version/g" $path/version.txt
-
-changelog=$(sed -e "s/\r//g" $path/amng-build/Changelog-NG.txt | sed -n "/$version/,/^$/{/./p}" | sed -e "s/$/\\\\n /g" | tr '\n' ' ')
 
     curl -i --silent \
         -H "Accept: application/json" \
@@ -79,7 +77,7 @@ changelog=$(sed -e "s/\r//g" $path/amng-build/Changelog-NG.txt | sed -n "/$versi
             }]
         }' $WEBHOOK > /dev/null
 
-        echo "Discord send"
+        echo "Build notification sent"
 
 else
 
@@ -100,7 +98,7 @@ else
             }]
         }' $WEBHOOK > /dev/null
 
-        echo "Discord send"
+        echo "Build notification sent"
 
 fi
 fi
