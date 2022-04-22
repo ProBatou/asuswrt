9195e6579d3516acf22096b21d025a3a2b041d2c
ProBatou <baptiste2105@hotmail.fr>
2022-04-22 11:41:10 +0200

    beautify

--- compile_asuswrt.sh
+++ compile_asuswrt.sh
@@ -2,7 +2,7 @@
 
 BOTNAME=Build-Notify
 AVATAR_URL="https://a.fsdn.com/allura/p/asuswrt-merlin/icon?1561187555?&w=90"
-getCurrentTimestamp() { date -u --iso-8601=seconds; };
+getCurrentTimestamp() { date -u --iso-8601=seconds; }
 path=$PWD
 
 if [ -f "$path/WEBHOOK.txt" ]; then
@@ -11,20 +11,20 @@ if [ -f "$path/WEBHOOK.txt" ]; then
 else
     touch $path/WEBHOOK.txt
     read -p "what is the url of the webhook"$'\n'
-    echo $REPLY >> WEBHOOK.txt
+    echo $REPLY >>WEBHOOK.txt
 fi
 
 if [ -d "$path/version.txt" ]; then
     latestVersion=$(cat $path/version.txt)
     echo "file exist"
 else
-  touch $path/version.txt
-  echo "000" > version.txt
-  latestVersion=$(cat $path/version.txt)
-  echo "file created"
+    touch $path/version.txt
+    echo "000" >version.txt
+    latestVersion=$(cat $path/version.txt)
+    echo "file created"
 fi
 
-version=$(curl --silent https://www.asuswrt-merlin.net/  | perl -ln0e '/<table.*?table>/s;print $&' | grep -A 3 -B 1 RT-AX56U | html2text | sed '2!d')
+version=$(curl --silent https://www.asuswrt-merlin.net/ | perl -ln0e '/<table.*?table>/s;print $&' | grep -A 3 -B 1 RT-AX56U | html2text | sed '2!d')
 
 if [ "$latestVersion" = "$version" ]; then
     echo "Version is equal"
@@ -32,80 +32,80 @@ else
 
     echo "New version available"
 
-#wget -q --show-progress https://codeload.github.com/RMerl/asuswrt-merlin.ng/tar.gz/refs/tags/$version
+    #wget -q --show-progress https://codeload.github.com/RMerl/asuswrt-merlin.ng/tar.gz/refs/tags/$version
 
-cp ../$version $path/$version
+    cp ../$version $path/$version
 
-echo "Downlad finished"
+    echo "Downlad finished"
 
-if [ -d "$path/amng-build/" ]; then
-  echo "Folder exist"
-else
-  mkdir $path/amng-build
-  echo "Folder created"
-fi
+    if [ -d "$path/amng-build/" ]; then
+        echo "Folder exist"
+    else
+        mkdir $path/amng-build
+        echo "Folder created"
+    fi
 
-pv $version | tar -xzf - --strip 1 -C $path/amng-build
+    pv $version | tar -xzf - --strip 1 -C $path/amng-build
 
-rm $path/$version
+    rm $path/$version
 
-echo "Archive extracted"
+    echo "Archive extracted"
 
-find $path/amng-build/ -type f | xargs grep -l -s "X-Frame-Options" | xargs sed -i '/X-Frame-Options/d'
-find $path/amng-build/ -type f | xargs grep -l -s "x-frame-options" | xargs sed -i '/x-frame-options/d'
-find $path/amng-build/ -type f | xargs grep -l -s "x-xss-protection" | xargs sed -i '/x-xss-protection/d'
-find $path/amng-build/ -type f | xargs grep -l -s "top.location.href" | xargs sed -i 's/top.location.href/window.location.href/g'
-find $path/amng-build/ -type f | xargs grep -l -s " window.top" | xargs sed -i 's/ window.top/ window/g'
-find $path/amng-build/ -type f | xargs grep -l -s "top.document" | xargs sed -i 's/top.document/document/g'
-find $path/amng-build/ -type f | xargs grep -l -s "top.isIE8" | xargs sed -i 's/top.isIE8/isIE8/g'
+    find $path/amng-build/ -type f | xargs grep -l -s "X-Frame-Options" | xargs sed -i '/X-Frame-Options/d'
+    find $path/amng-build/ -type f | xargs grep -l -s "x-frame-options" | xargs sed -i '/x-frame-options/d'
+    find $path/amng-build/ -type f | xargs grep -l -s "x-xss-protection" | xargs sed -i '/x-xss-protection/d'
+    find $path/amng-build/ -type f | xargs grep -l -s "top.location.href" | xargs sed -i 's/top.location.href/window.location.href/g'
+    find $path/amng-build/ -type f | xargs grep -l -s " window.top" | xargs sed -i 's/ window.top/ window/g'
+    find $path/amng-build/ -type f | xargs grep -l -s "top.document" | xargs sed -i 's/top.document/document/g'
+    find $path/amng-build/ -type f | xargs grep -l -s "top.isIE8" | xargs sed -i 's/top.isIE8/isIE8/g'
 
-echo "Patch applied"
+    echo "Patch applied"
 
-start=`date +%s`
-cd $path/amng-build/release/src-rt-5.02axhnd.675x/ && /usr/bin/make -s --no-print-directory rt-ax56u
-error=$?
-end=`date +%s`
+    start=$(date +%s)
+    cd $path/amng-build/release/src-rt-5.02axhnd.675x/ && /usr/bin/make -s --no-print-directory rt-ax56u
+    error=$?
+    end=$(date +%s)
 
-runtime=$((end-start))
+    runtime=$((end - start))
 
-if (( $runtime > 3600 )) ; then
-    let "hours=runtime/3600"
-    let "minutes=(runtime%3600)/60"
-    let "seconds=(runtime%3600)%60"
-    runtime="in $hours hour(s), $minutes minute(s) and $seconds second(s)" 
-elif (( $runtime > 60 )) ; then
-    let "minutes=(runtime%3600)/60"
-    let "seconds=(runtime%3600)%60"
-    runtime="in $minutes minute(s) and $seconds second(s)"
-else
-    runtime="in $runtime seconds"
-fi
+    if (($runtime > 3600)); then
+        let "hours=runtime/3600"
+        let "minutes=(runtime%3600)/60"
+        let "seconds=(runtime%3600)%60"
+        runtime="in $hours hour(s), $minutes minute(s) and $seconds second(s)"
+    elif (($runtime > 60)); then
+        let "minutes=(runtime%3600)/60"
+        let "seconds=(runtime%3600)%60"
+        runtime="in $minutes minute(s) and $seconds second(s)"
+    else
+        runtime="in $runtime seconds"
+    fi
 
-if [ -d "/var/www/html/asuswrt" ]; then
-    sudo rm -rf /var/www/html/asuswrt/*
-    echo "Folder exist remove file in it"
-else
-    sudo mkdir /var/www/html/asuswrt
-    echo "Folder created"
-fi
+    if [ -d "/var/www/html/asuswrt" ]; then
+        sudo rm -rf /var/www/html/asuswrt/*
+        echo "Folder exist remove file in it"
+    else
+        sudo mkdir /var/www/html/asuswrt
+        echo "Folder created"
+    fi
 
-sudo cp $(find $path/amng-build/release/src-rt-5.02axhnd.675x/ -name *_cferom_pureubi.w) /var/www/html/asuswrt/
+    sudo cp $(find $path/amng-build/release/src-rt-5.02axhnd.675x/ -name *_cferom_pureubi.w) /var/www/html/asuswrt/
 
-changelog=$(sed -e "s/\r//g" $path/amng-build/Changelog-NG.txt | sed -n "/$version/,/^$/{/./p}" | sed -e "s/$/\\\\n /g" | tr '\n' ' ')
+    changelog=$(sed -e "s/\r//g" $path/amng-build/Changelog-NG.txt | sed -n "/$version/,/^$/{/./p}" | sed -e "s/$/\\\\n /g" | tr '\n' ' ')
 
-echo "Build finished"
+    echo "Build finished"
 
-if [ $error = 0 ]; then
+    if [ $error = 0 ]; then
 
-sudo rm -rf $path/amng-build/
+        sudo rm -rf $path/amng-build/
 
-sed -i "s/$latestVersion/$version/g" $path/version.txt
+        sed -i "s/$latestVersion/$version/g" $path/version.txt
 
-    curl -i --silent \
-        -H "Accept: application/json" \
-        -H "Content-Type:application/json" \
-        -X POST \
-        --data  '{
+        curl -i --silent \
+            -H "Accept: application/json" \
+            -H "Content-Type:application/json" \
+            -X POST \
+            --data '{
             "username": "'"$BOTNAME"'",
             "avatar_url": "'"$AVATAR_URL"'",
             "embeds": [{
@@ -116,17 +116,17 @@ sed -i "s/$latestVersion/$version/g" $path/version.txt
                 "description": "New update for router RT-AX56U\n\n**Patch Note: ** '"$changelog"'\n",
                 "timestamp": "'$(getCurrentTimestamp)'"
             }]
-        }' $WEBHOOK > /dev/null
+        }' $WEBHOOK >/dev/null
 
         echo "Build notification sent"
 
-else
+    else
 
-    curl -i --silent \
-        -H "Accept: application/json" \
-        -H "Content-Type:application/json" \
-        -X POST \
-        --data  '{
+        curl -i --silent \
+            -H "Accept: application/json" \
+            -H "Content-Type:application/json" \
+            -X POST \
+            --data '{
             "username": "'"$BOTNAME"'",
             "avatar_url": "'"$AVATAR_URL"'",
             "embeds": [{
@@ -137,9 +137,9 @@ else
                 "description": "Build fail \n",
                 "timestamp": "'$(getCurrentTimestamp)'"
             }]
-        }' $WEBHOOK > /dev/null
+        }' $WEBHOOK >/dev/null
 
         echo "Build notification sent"
 
-fi
+    fi
 fi
